2d6a58e5fd655b66a7dd1a9cda9b7a236a1e9743
Vinayak Sharma <vinayaksharma2041@gmail.com>
2026-09-03 09:33:05 +0530

    Fix merge conflict markers

--- Hello.java
+++ Hello.java
@@ -1,9 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-<<<<<<< HEAD
-    System.out.println("Change from main branch");
-=======
-    System.out.println("Change from conflict branch");
->>>>>>> conflict-practice
+        System.out.println("Change from main branch");
     }
 }
